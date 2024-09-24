65fbac4e627ccd900e93b475e2ab7998602ef320
Dio Briggs <diobriggs@icloud.com>
2024-09-24 18:08:47 -0400

    Initial project setup and structure

---
+++ README.md
@@ -0,0 +1,3 @@
+# weather_info_app
+
+A new Flutter project.
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(WeatherInfoApp());
+}
+
+class WeatherInfoApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Weather Info App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: WeatherHomePage(),
+    );
+  }
+}
+
+class WeatherHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Weather Info'),
+      ),
+      body: Center(
+        child: Text('Welcome to the Weather Info App'),
+      ),
+    );
+  }
+}
